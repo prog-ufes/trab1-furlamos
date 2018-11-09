ad17dd830f1bd827a55478f0832bf2e2b35bb5af
Leandro Furlam <leandrofturi@gmail.com>
2018-11-09 13:53:56 -0200

    adequacao do config

--- distancias.c
+++ distancias.c
@@ -2,49 +2,69 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define N 40
+
 // IMPLEMENTACAO DO CALCULO DAS DISTANCIAS
 
 // DISTANCIA EUCLIDIANA
-float distEucl (float *p, float *q, int n){
+float distEucl (float *p, float *q, int n) {
 	float dist = 0;
-	for(int i = 0; i < n; i++)
-		dist += pow(p[i] - q[i], 2);
-	dist = sqrt(dist);
+	for (int i = 0; i < n; i++) {
+		dist += pow (p[i] - q[i], 2);
+	}
+	dist = sqrt (dist);
 	return dist;
 }
 
 // DISTANCIA DE MINKOWSKY
-float distMink (float *p, float *q, int n, float r){
+float distMink (float *p, float *q, int n, float r) {
 	float dist = 0;
-	for(int i = 0; i < n; i++)
-		dist += pow(abs(p[i] - q[i]), r);
+	for (int i = 0; i < n; i++) {
+		dist += pow (abs (p[i] - q[i]), r);
+	}
 	dist = pow(dist, 1/r);
 	return dist;
 }
 
-// SIMILARIDADE DE COSSENOS
-float simCos (float *p, float *q, int n){
+// DISTANCIA DE CHEBYSHEV
+float distCheb (float *p, float *q, int n) {
 	float dist = 0;
-	for(int i = 0; i < n; i++)
-		dist += p[i] * q[i];
-	dist = dist / distEucl(p, q, n);
+	for (int i = 0; i < n; i++) {
+		if (abs(p[i] - q[i]) > dist) {
+			dist = abs (p[i] - q[i]);
+		}
+	}
 	return dist;
 }
 
-int main(){
-	char dist;
-	switch(dist){
-		case 'E':
-		break;
+// LEITURA DO ARQUIVO config.txt
+int leituraConfig (char *treino, char *teste, char *saida) {
+	FILE *config;
+	treino = (char*) malloc (N * sizeof (char));
+	teste = (char*) malloc (N * sizeof (char));
+	saida = (char*) malloc (N * sizeof (char));
 
-		case 'M':
-		break;
+	config = fopen ("config.txt", "r");
+	if(config == NULL) {
+      perror ("Error opening file");
+      fclose (config); //tem que dar free nos ponteiros
+      return(1);
+   	}
+  else {
+		fgets (treino, N, config);
+		fgets (teste, N, config);
+		fgets (saida, N, config);
 
-		case 'C':
-		break;
+		if (!(treino) || !(teste) || !(saida)) {
+			fclose (config);
+			return(1);
+		}
+  }
+   	fclose (config);
 
-		default:
-		break;
-	}
+	return 0;
+}
+
+int main () {
 	return 0;
 }

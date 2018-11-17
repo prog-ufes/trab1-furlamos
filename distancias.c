052a1b8052548eb9469d9065ca01cf6a1d6c5901
Leandro Furlam <leandrofturi@gmail.com>
2018-11-17 14:18:52 -0200

    ta indo

--- distancias.c
+++ distancias.c
@@ -4,43 +4,47 @@
 
 #define N 260
 
-
-
-
+float distancias (float *p, float *q, int n, float r, char tipo);
+int busca (float *vet, int n, float k);
+void bubbleSort (float *vet, int *pos, int n);
+int leitura (int k, char tipo, float r, char *path, int tipoLeitura);
+float classificador (int k, char tipo, float r, float *p, float **matBase, int qntCol, int qntL, float *rotulos, int qntRot, int tipoClass);
+void confusao (int **matConfusa, float rotClass, float rotReal, float *rotulos, int qntRot);
+float acuracia (int **matConfusa, int qntRot);
 
 
 // IMPLEMENTACAO DO CALCULO DAS DISTANCIAS
-
-// DISTANCIA EUCLIDIANA
-float distEucl (float *p, float *q, int n) {
-	float dist = 0;
-	for (int i = 0; i < n; i++) {
-		dist += pow (p[i] - q[i], 2);
-	}
-	dist = sqrt (dist);
-	return dist;
-}
-
-// DISTANCIA DE MINKOWSKY
-float distMink (float *p, float *q, int n, float r) {
+float distancias (float *p, float *q, int n, float r, char tipo) {
 	float dist = 0, mod = 0;
-	for (int i = 0; i < n; i++) {
-		mod = sqrt (pow ((p[i] - q[i]), 2));
-		dist += pow (mod, r);
-	}
-	dist = pow(dist, 1/r);
-	return dist;
-}
+	switch (tipo) {
+			case 'E':
+				for (int i = 0; i < n; i++) {
+					dist += pow (p[i] - q[i], 2);
+				}
+				dist = sqrt (dist);
+			break;
 
-// DISTANCIA DE CHEBYSHEV
-float distCheb (float *p, float *q, int n) {
-	float dist = 0;
-	for (int i = 0; i < n; i++) {
-		if ((pow ((p[i] - q[i]), 2)) > dist) {
-			dist = pow ((p[i] - q[i]), 2);
+			case 'M':
+				for (int i = 0; i < n; i++) {
+					mod = sqrt (pow ((p[i] - q[i]), 2));
+					dist += pow (mod, r);
+				}
+				dist = pow(dist, 1/r);
+			break;
+
+			case 'C':
+				for (int i = 0; i < n; i++) {
+					if ((pow ((p[i] - q[i]), 2)) > dist) {
+						dist = pow ((p[i] - q[i]), 2);
+					}
+				}
+				dist = sqrt (dist);
+			break;
+
+			default:
+				printf("Distancia desconhecida!\n");
+				exit (1);
 		}
-	}
-	dist = sqrt (dist);
 	return dist;
 }
 
@@ -53,17 +57,41 @@ int busca (float *vet, int n, float k) {
 	return 1;
 }
 
+void bubbleSort (float *vet, int *pos, int n) {
+	int aux;
+	for (int i = 0; i < n - 1; i ++) {
+		for (int j = i + 1; j < n; j ++) {
+			if (vet[j] < vet[i]) {
+				aux = pos[i];
+                pos[i] = pos[j];
+                pos[j] = aux;
+			}
+		}
+	}
+}
 
+int maior (int *vet, int n) {
+	int posMaior, maior = 0;
+	for (int i = 0; i < n; i ++) {
+		if (vet[i] > maior) {
+			maior = vet[i];
+			posMaior = i;
+		}
+	}
+	return posMaior;
+}
 
 // LEITURA DO treino/teste
-int leitura (char *path, int tipoLeitura) { //1 para treino, 2 para teste
-	int qntB = 0, qntCol = 0, qntL = 0, qntRot = 0;
+int leitura (int k, char tipo, float r, char *path, int tipoLeitura) { //1 para treino, 2 para teste
+	int **matConfusa,
+	    qntB = 0, qntCol = 0, qntL = 0, qntRot = 0;
     char linha;
     float **matBase,
-		  *rotulos, *base;
+		  *rotulos, *base,
+		  rotReal, rotClass;
 	FILE *treiste;
 
-	treiste = fopen ("iris_treino.csv", "r");
+	treiste = fopen ("iris_teste.csv", "r");
 
 	if (treiste == NULL) {
 	    printf ("Erro ao abrir o arquivo!\n");
@@ -82,16 +110,14 @@ int leitura (char *path, int tipoLeitura) { //1 para treino, 2 para teste
 	qntL --;
     qntCol = (qntB - 1) / qntL;
 	matBase = (float**) malloc (qntL * (sizeof (float*)));
-	*matBase = (float*) malloc (qntCol * (sizeof (float)));
 
-	for (int k = 0, i = 0, j = 0; k < (qntB - 1); k ++, j ++) {
-		if ((j % qntCol == 0) && (j != 0)) {
+	for (int k = 0, i = -1, j = 0; k < (qntB - 1); k ++, j ++) {
+		if (j % qntCol == 0) {
 			i ++;
 			j = 0;
 			matBase[i] = (float*) malloc (qntCol * (sizeof (float)));
-			matBase[i][j] = base[k];
 
-			if (tipoLeitura == 1) {
+			if ((tipoLeitura == 1) && (k != 0)) {
 				if (qntRot == 0) {
 					rotulos = (float*) malloc (sizeof (float));
 					rotulos[0] = base[k - 1];
@@ -102,40 +128,85 @@ int leitura (char *path, int tipoLeitura) { //1 para treino, 2 para teste
 					rotulos = realloc (rotulos, qntRot * sizeof (float));
 					rotulos[qntRot - 1] = base[k - 1];
 				}
-				// começa a matriz de confusão
 			}
 		}
+
+		matBase[i][j] = base[k];
 	}
   	free (base);
 
-	// aqui vai a matriz de confusão e o teste
+	matConfusa = (int**) malloc (qntRot * (sizeof (int*)));
+	for (int i = 0; i < qntRot; i ++) {
+		matConfusa[i] = (int*) malloc (qntRot * (sizeof (int)));
+		for (int j = 0; j < qntRot; j ++) matConfusa[i][j] = 0;
+	}
 
 	for (int i = 0; i < qntL; i ++) {
-		free (matBase[i]);
+		rotReal = matBase[i][qntCol - 1];
+		rotClass = classificador (k, tipo, r, matBase[i], matBase, qntCol, qntL, rotulos, qntRot, 1);
+		confusao (matConfusa, rotClass, rotReal, rotulos, qntRot);
 	}
+
+	for (int i = 0; i < qntL; i ++) free (matBase[i]);
 	free (matBase);
+	for (int i = 0; i < qntRot; i ++) free (matConfusa[i]);
+	free (matConfusa);
 	free (rotulos);
     return 0;
 }
 
 
-int **matConfusa (float **matBase, int qntCol, int qntL, float *rotulos, int qntRot, int inicial) { //lembrar de enviar qntCol - 1, pra tirar o rotulo
-	int **confusa;
-	confusa = (int**) malloc (qntRot * sizeof (int*));
-	for (int i = 0; i < qntRot; i++) {
-		confusa[i] = (int*) malloc (qntRot * sizeof (int));
-	}
 
-	for (int i = 0; i < qntL; i++) {
-		for (int j = 0; j < qntCol; j++) {
-			/* code */
+float classificador (int k, char tipo, float r, float *p, float **matBase, int qntCol, int qntL, float *rotulos, int qntRot, int tipoClass) { //1 para treino, 2 tste
+	int *pos, *contaRot,
+	    posClass;
+	float *q, *dist,
+		  rotuloClass;
+
+	pos = (int*) malloc (sizeof (int));
+	dist = (float*) malloc (sizeof (float));
+
+	for (int i = 0; i < qntL; i ++) {
+		q = matBase[i];
+		pos = realloc (pos, (i + 1) * sizeof (int));
+		pos[i] = i;
+		dist = realloc (dist, (i + 1) * sizeof (float));
+		dist[i] = distancias (p, q, (qntCol - 1), r, tipo);
+	}
+	bubbleSort (dist, pos, qntL);
+	contaRot = (int*) malloc (qntRot * sizeof (int));
+	for (int i = 0; i < qntRot; i ++) contaRot[i] = 0;
+	if (tipoClass) k ++;
+	for (int i = 0; i < k; i ++) {
+		if (tipoClass) i ++;
+		for (int j = 0; j < qntRot; j ++) {
+			if (matBase[pos[i]][qntCol - 1] == rotulos[j]) contaRot[j] ++;
 		}
 	}
-	return confusa;
+	posClass = maior (contaRot, qntRot);
+	rotuloClass = rotulos[posClass];
+	return rotuloClass;
 }
 
+float acuracia (int **matConfusa, int qntRot) {
+	int acertos = 0, total = 0;
+	float acc;
+	for (int i = 0; i < qntRot; i ++) {
+		acertos += matConfusa[i][i];
+		for (int j = 0; j < qntRot; j ++) total += matConfusa[i][j];
+	}
+	acc = acertos / total;
+	return acc;
+}
 
-
+void confusao (int **matConfusa, float rotClass, float rotReal, float *rotulos, int qntRot) {
+	int m, n;
+	for (int i = 0; i < qntRot; i ++) {
+		if (rotReal == rotulos[i]) m = i;
+		if (rotClass == rotulos[i]) n = i;
+	}
+	matConfusa[m][n] ++;
+}
 
 // LEITURA DO ARQUIVO config.txt
 int main () {
@@ -164,30 +235,17 @@ int main () {
 
 		if ((*pathTreino == '\n') || (*pathTeste == '\n') || (*pathSaida == '\n')) {
 			printf ("Arquivo incorreto!\n");
-			fclose (config);
-			free (pathTreino);
-			free (pathTeste);
-			free (pathSaida);
 			exit(1);
 		}
 
 		while (! feof (config)) { //ESTA FAZENDO O FINAL DUAS VEZES
 			fscanf (config, "%d, %c, %f", &k, &tipo, &r);
 
-			if (leitura (pathTreino, 1)) {
-				printf ("Arquivo incorreto!\n");
-				fclose (config);
-				free (pathTreino);
-				free (pathTeste);
-				free (pathSaida);
-				exit(1);
+			if (leitura (k, tipo, r, pathTreino, 1)) {
+
 			}
-			//FAZER A MATRIZ DE CONFUSÃO PARA OS TESTES
 		}
 	}
 	fclose (config);
-	free (pathTreino);
-	free (pathTeste);
-	free (pathSaida);
 	return 0;
 }

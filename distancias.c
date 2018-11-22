53dc4d5cb374182b5e1314b69627303a570c3bf7
Leandro Furlam <leandrofturi@gmail.com>
2018-11-21 22:20:03 -0200

    so colocar o nome na saida e sucesso

--- distancias.c
+++ distancias.c
@@ -5,16 +5,27 @@
 
 #define N 260
 
+struct rotulo {
+	float dist;
+	float rotClass;
+};
+
 int leitura (float ***matBase, char *path, int *qntL, int *qntCol, float *rotMax);
 float distancias (float *p, float *q, int n, float r, char tipo);
+float classificador (int k, char tipo, float r, float *p, float **matTreino, int qntL, int qntCol, float rotMax);
+void bubbleSort (struct rotulo *distancia, int n);
+void confusao (int **matConfusa, float rotClass, float rotReal, float rotMax);
+float acuracia (int **matConfusa, int qntRot);
 
 int main () {
-    int qntL, qntLTeste, qntCol, k;
+    int **matConfusa,
+    	qntL, qntLTeste, qntCol, k;
 	char pathTreino[N], pathTeste[N], pathSaida[N], novoPathSaida[N],
 		 tipo;
     float **matTreino = NULL, **matTeste = NULL,
-          rotMax = 0, r;
-	FILE *config;
+    	  *p,
+          rotMax = 0, r, rotReal, acc;
+	FILE *config, *saida;
 
 	config = fopen ("config.txt", "r");
 
@@ -28,18 +39,45 @@ int main () {
         fscanf (config, "%s\n", pathTreino);
         fscanf (config, "%s\n", pathTeste);
         fscanf (config, "%s\n", pathSaida);
-
     }
 
     if(! (leitura (&matTreino, pathTreino, &qntL, &qntCol, &rotMax))) {
         if(! (leitura (&matTeste, pathTeste, &qntLTeste, &qntCol, &rotMax))) {
 
-            int matConfusa[(int) rotMax][(int) rotMax];
+        	float rotClass[qntLTeste];
+            matConfusa = (int**) malloc (((int) rotMax) * sizeof (int*));
+            for (int i = 0; i < (int) rotMax; i ++) matConfusa[i] = (int*) malloc (((int) rotMax) * sizeof (int));
+
             fscanf (config, "%d, %c, %f", &k, &tipo, &r);
             for (int s = 1; (! feof (config)); s++) {
                 for (int i = 0; i < ((int) rotMax); i ++) {
                     for (int j = 0; j < ((int) rotMax); j ++) matConfusa[i][j] = 0;
                 }
+
+            	for (int i = 0; i < qntLTeste; i ++) {
+            		p = matTeste[i];
+            		rotReal = matTeste[i][qntCol - 1];
+            		rotClass[i] = classificador (k, tipo, r, p, matTreino, qntL, qntCol, rotMax);
+            		confusao (matConfusa, rotClass[i], rotReal, rotMax);
+            	}
+            	acc = acuracia (matConfusa, ((int) rotMax));
+
+
+            	saida = fopen ("pathSaida", "w");
+            	fprintf (saida, "%.2f\n\n", acc);
+            	for (int i = 0; i < (int) rotMax; i ++) {
+            		for (int j = 0; j < (int) rotMax; j ++) {
+            			if (j != 0) fprintf (saida, " ");
+            			fprintf (saida, "%d", matConfusa[i][j]);
+            		}
+            		fprintf (saida, "\n");
+            	}
+            	fprintf (saida, "\n");
+				for (int i = 0; i < qntLTeste; i ++) {
+					if (i != 0) fprintf (saida, "\n");
+					fprintf (saida, "%.2f", rotClass[i]);
+				}
+				fclose (saida);
 
                 fscanf (config, "%d, %c, %f", &k, &tipo, &r);
             }
@@ -48,10 +86,12 @@ int main () {
         }
         for (int i = 0; i < qntLTeste; i++) free (matTeste[i]);
         free (matTeste);
+
+    	for (int i = 0; i < (int) rotMax; i ++) free (matConfusa[i]);
+    	free (matConfusa);
     }
     for (int i = 0; i < qntL; i++) free (matTreino[i]);
     free (matTreino);
-
     return 0;
 }
 
@@ -132,4 +172,72 @@ float distancias (float *p, float *q, int n, float r, char tipo) {
 				exit (1);
 		}
 	return dist;
+}
+
+float classificador (int k, char tipo, float r, float *p, float **matTreino, int qntL, int qntCol, float rotMax) {
+	float *q,
+		  rotClass = 0, rotAtual;
+	struct rotulo *distancia, contaRot[(int) rotMax];
+	distancia = (struct rotulo*) malloc (qntL * sizeof (struct rotulo));
+	for (int i = 0; i < (int) rotMax; i ++) contaRot[i].dist = 0;
+
+	for (int i = 0; i < qntL; i ++) {
+		q = matTreino[i];
+		distancia[i].dist = distancias (p, q, (qntCol - 1), r, tipo);
+		distancia[i].rotClass = q[qntCol - 1];
+	}
+	bubbleSort (distancia, (qntL));
+	for (int i = 0; i < k; i ++) {
+		rotAtual = 1.0;
+		for (int j = 0; j < (int) rotMax; j ++) {
+			if (distancia[i].rotClass == rotAtual) {
+				contaRot[j].dist ++;
+				contaRot[j].rotClass = rotAtual;
+			}
+			rotAtual ++;
+		}
+	}
+
+	bubbleSort (contaRot, (int) rotMax);
+	rotClass = contaRot[((int) rotMax) - 1].rotClass;
+	free (distancia);
+	return rotClass;
+}
+
+
+void bubbleSort (struct rotulo *distancia, int n) {
+	struct rotulo aux;
+	for (int i = 0; i < n; i ++) {
+		for (int j = i + 1; j < n; j ++) {
+			if (distancia[j].dist < distancia[i].dist) {
+				aux = distancia[i];
+                distancia[i] = distancia[j];
+                distancia[j] = aux;
+			}
+		}
+	}
+}
+
+void confusao (int **matConfusa, float rotClass, float rotReal, float rotMax) {
+	int m, n;
+	for (float j = 1.0; j <= rotMax; j ++) {
+		if (rotReal == j) {
+			m = ((int) j) - 1;
+		}
+		if (rotClass == j) {
+			n = ((int) j) - 1;
+		}
+	}
+	matConfusa[m][n] ++;
+}
+
+float acuracia (int **matConfusa, int qntRot) {
+	int acertos = 0, total = 0;
+	float acc;
+	for (int i = 0; i < qntRot; i ++) {
+		acertos += matConfusa[i][i];
+		for (int j = 0; j < qntRot; j ++) total += matConfusa[i][j];
+	}
+	acc = (float) acertos / (float) total;
+	return acc;
 }
